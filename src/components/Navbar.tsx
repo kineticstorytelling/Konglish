3fe52e21225b3cb01d3a3eaf4fa024333f3cfb40
kineticstorytelling <kineticstorytelling@gmail.com>
2025-01-06 22:50:38 +0900

    added bgs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,21 @@ import Link from 'next/link'
 import { Alegreya } from 'next/font/google'
 
 const alegreya = Alegreya({ subsets: ['latin'] })
+
 export default function Navbar() {
   return (
-    <nav className="bg-light-gray text-black shadow-lg">
+    <nav className="bg-opacity-15 bg-amber-200 text-black shadow-lg">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center">
-          <Link href="/" className="py-4 flex items-center">
-            <img src="/assets/logos/my-logo-black-removebg.png" alt="Konglish Logo" className="h-24 w-auto mr-4" />
-            <div className="text-4xl">
+          <Link href="/" className="py-2 flex flex-col items-center">
+            <img src="/assets/logos/konglish-logo.png" alt="Konglish Logo" className="h-20 w-auto" />
+            {/* <div className="text-4xl">
               <span className="font-bold font-ryukosei text-5xl">Ko</span>
               <span className={`${alegreya.className} font-bold`}>nglish</span>
-            </div>
+            </div> */}
           </Link>
 
-          <div className={`${alegreya.className} flex space-x-8 py-4 text-3xl`} >
+          <div className={`${alegreya.className} flex space-x-8 py-4 text-3xl`}>
             <Link href="/" className="hover:text-gray-300 transition-colors">
               Home
             </Link>
