fbc4cd638078f1b2e73f773aefc2834ca5d2edeb
kineticstorytelling <kineticstorytelling@gmail.com>
2025-01-13 18:32:01 +0900

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Alegreya } from 'next/font/google'
 import localFont from 'next/font/local'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const alegreya = Alegreya({ 
+  subsets: ['latin'],
+  variable: '--font-alegreya'
+})
 
+// Local fonts
 const ryujinAttack = localFont({ 
   src: './fonts/ryujin-attack.regular.ttf',
   variable: '--font-ryujin-attack'
 })
-const gingsul = localFont({ 
-  src: './fonts/gingsul-demo.regular.ttf',
-  variable: '--font-gingsul'
-})
+// const gingsul = localFont({ 
+//   src: '../../public/assets/fonts/gingsul.ttf',
+//   variable: '--font-gingsul'
+// })
 const ryukosei = localFont({ 
   src: '../../public/assets/fonts/ryukosei.otf',
   variable: '--font-ryukosei'
@@ -22,7 +26,7 @@ const ryukosei = localFont({
 
 export const metadata: Metadata = {
   title: 'Konglish',
-  description: 'Learn about Korean-English language fusion',
+  description: 'Exploring the connection between Korean and English',
 }
 
 export default function RootLayout({
@@ -31,8 +35,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${ryukosei.variable}`}>
-      <body className={inter.className}>
+    <html lang="en" className={`${alegreya.variable} ${ryukosei.variable}`}>
+      <body className={alegreya.className}>
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-8">
           {/* <h1 className="font-gingsul text-4xl mb-4">Welcome to Konglish</h1> */}
